9921d0bfb2cdd9dec8080342f354de561a5709a0
Jia <pochialu@gmail.com>
2020-07-23 17:19:04 +0800

    add new project

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+const app = express()
+const port = 3000
+
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+// homepage
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+// show target restaurant info
+app.get('/restaurants/:id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
+  res.render('show', { restaurant })
+})
+
+// search function
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => { return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) })
+  res.render('index', { restaurants, keyword })
+})
+
+
+app.listen(port, () => {
+  console.log(`express is running on http://localhost:${port}`)
+})
