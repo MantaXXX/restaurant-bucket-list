ac4a71711f6aed24fe78a1065756be38b88ca605
Jia <pochialu@gmail.com>
2020-07-31 15:06:11 +0800

    feat: add edit function

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const exphbs = require('express-handlebars')
 const Restaurant = require('./models/restaurant')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 
 const mongoose = require('mongoose')
+const { urlencoded } = require('body-parser')
 mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
@@ -18,7 +20,7 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({ extended: true }))
 // homepage
 app.get('/', (req, res) => {
   Restaurant.find()
@@ -36,11 +38,46 @@ app.get('/restaurants/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
-// search function
+// search
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => { return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) })
   res.render('index', { restaurants, keyword })
+})
+
+// edit
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const category = req.body.category
+  const location = req.body.location
+  const google_map = req.body.google_map
+  const phone = req.body.phone
+  const rating = req.body.rating
+  const description = req.body.description
+  const image = req.body.image
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.category = category
+      restaurant.location = location
+      restaurant.google_map = google_map
+      restaurant.phone = phone
+      restaurant.rating = rating
+      restaurant.description = description
+      restaurant.image = image
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
 })
 
 
